dd86dcab6f38518fe6d91cd8dab85f2adecb8e11
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-19 19:42:58 +0200

    Install cors and configure allowedOrigins

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,11 @@
-const aa = 3;
+import cors from "cors";
+import express from "express";
 
-export default aa;
+export const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://202304-w7chwe-robert-belenguer-front.netlify.app",
+];
+
+app.use(cors({ origin: allowedOrigins }));
