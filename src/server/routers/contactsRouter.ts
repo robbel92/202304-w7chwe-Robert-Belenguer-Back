37f63d37ccd4945dd4aca13a7c8b4edd5eb4da7e
Robertt Belenguer Cotaina <rob.belenguer@gmail.com>
2023-05-22 18:51:29 +0200

    Create contactsController

--- src/server/routers/contactsRouter.ts
+++ src/server/routers/contactsRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import { getContacts } from "../controllers/contacts/contactsController";
 
 const contactsRouter = Router();
 
-contactsRouter.get("/", getConta);
+contactsRouter.get("/", getContacts);
 
 export default contactsRouter;
